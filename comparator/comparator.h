61fc1838fe215a28c09cf8e7471000205d47dd10
Andrey <agar10@yandex.ru>
2023-09-16 20:38:12 +0300

    TRY to build.

---
+++ comparator/comparator.h
@@ -0,0 +1,11 @@
+#include <algorithm>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+// Custom Comparator to sort the array in increasing order
+bool customComparator(string, string);
+
+// Function to sort the IP Addresses
+vector<string> sortIPAddress(vector<string>);
