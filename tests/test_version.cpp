5a4e557d2d65902dd4a59158fdb361c504168d2f
Andrey <agar10@yandex.ru>
2023-09-16 19:18:00 +0300

    Added tests/ to Git tracking.

---
+++ tests/test_version.cpp
@@ -0,0 +1,26 @@
+/*
+#define BOOST_TEST_MODULE test_version
+
+#include "lib.h"
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(test_version)
+
+BOOST_AUTO_TEST_CASE(test_valid_version)
+{
+    BOOST_CHECK(version() != 100);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+*/
+#include <iostream>
+#include <gtest/gtest.h>
+#include <MakeIP/makeip.h>
+#include <version/lib.h>
+
+TEST(TEST_GOOGLETEST, Test1)
+{
+    std::cout<<"version="<<version()<<std::endl;
+    ASSERT_EQ(version(), 100);
+}
