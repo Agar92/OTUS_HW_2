5713e51b0589756d5f5c933edf363782e7696534
Andrey <agar10@yandex.ru>
2023-11-18 10:13:11 +0300

    Revert "remove all files for review"
    This reverts commit e710463a76ea9f052c56905e6a03dfb308f9bca8.

---
+++ comparator/comparator.h
@@ -0,0 +1,12 @@
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using std::string;
+using std::vector;
+
+// Custom Comparator to sort the array in increasing order
+bool customComparator(string, string);
+
+// Function to sort the IP Addresses
+vector<string> sortIPAddress(vector<string>);
---
+++ tests/script.sh
@@ -0,0 +1,41 @@
+#set -eu
+
+#pwd
+#exit 0
+FILE="ip_filter"
+#DATAFILE="../OTUS_HW2/ip_filter.tsv"
+DATAFILE="$1"
+echo "Hello! DATAFILE=${DATAFILE}"
+echo "DATAFILE=${DATAFILE}"
+OUTPUTFILE="output.dat"
+EXPECTED_RESULT="24e7a7b2270daee89c64d3ca5fb3da1a  output.dat"
+
+if [ -f $FILE ]; then
+    echo "File found!"
+    if [ -f $DATAFILE ]; then
+      echo "$DATAFILE found!"
+    fi
+    echo "STEP #1 DATAFILE=${DATAFILE}"
+    ./$FILE $DATAFILE > $OUTPUTFILE
+    echo "STEP #2"
+    result=$(md5sum $OUTPUTFILE)
+    md5sum --v
+    if [ -f $OUTPUTFILE ]; then
+      echo "$OUTPUTFILE found!"
+    fi
+    echo "result=${result}"
+    echo "result=${result} EXPECTED_RESULT=${EXPECTED_RESULT}"
+#    exit 0
+    if [ "$result" = "$EXPECTED_RESULT" ]; then
+      echo "Check sums are the same. Good!"
+      exit 0
+    else
+      echo "Check sums are different."
+      exit 1
+    fi
+else
+    echo "Binary file not found! Test failed!"
+    exit 1
+fi
+
+exit 0
