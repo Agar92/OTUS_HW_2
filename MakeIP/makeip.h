e710463a76ea9f052c56905e6a03dfb308f9bca8
Andrey <agar10@yandex.ru>
2023-11-18 10:06:26 +0300

    remove all files for review

--- MakeIP/makeip.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <string>
-
-unsigned int MakeIP(std::string oct1, std::string oct2,
-                    std::string oct3, std::string oct4);
-
-std::string MakeIP(unsigned int ip);
